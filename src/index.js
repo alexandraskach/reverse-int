b32f17d726c1c5924a057b6fbe075eaf07f6a81c
Aliaksandra Skachkova <skach.alexandra@gmail.com>
2021-02-09 07:20:53 +0100

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let result = (n) => parseInt(String(n).split("").reverse().join(""), 10);
+    return result(n);
+};
